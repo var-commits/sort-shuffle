5d62a2b222e7973103e72ce69532533f80470124
var-commits <varunsankar@outlook.com>
2021-09-23 19:57:29 +0530

    Fine tuning

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 let cardsList = ["1", "2", "3", "4", "5", "6", "7", "8", "9"],
   cardsContainer = document.getElementById("cards-container");
 
-cardsList.forEach((item, index) => {
+cardsList.forEach((item, i) => {
   cardsContainer.innerHTML += "<li data-item=" + item + ">" + item + "</li>";
 });
 
